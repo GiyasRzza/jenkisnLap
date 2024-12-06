2224b89a905fee4cd671f31b37a4d180d2a53d5d
GiyasRzza <giyassrzza@gmail.com>
2024-12-06 19:07:32 +0400

    test

---
+++ src/main/java/org/example/jenkisnlap/JenkisnLapApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkisnlap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisnLapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkisnLapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jenkisnlap/JenkisnLapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jenkisnlap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkisnLapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
